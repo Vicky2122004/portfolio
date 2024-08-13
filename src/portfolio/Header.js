8e61f51c8157d792027dd212d548e23e1d5d29c4
Marcsiva <sivakumar20011005@gmail.com>
2024-08-13 07:45:10 +0530

    portfolio folder modified

--- src/portfolio/Header.js
+++ src/portfolio/Header.js
@@ -1,17 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./Header.css";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { RiMenu2Fill } from "react-icons/ri";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { IoMdClose } from "react-icons/io";
+import { FaHome } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { HiMiniWrenchScrewdriver } from "react-icons/hi2";
+import { FaBriefcase } from "react-icons/fa";
+import { MdContactMail } from "react-icons/md";
 
 export const Header = () => {
   const [menu, setMenu] = useState(false);
 
-  const [active, setActive] = useState('')
+  const [activeLink, setActiveLink] = useState("#home");
 
-  function handleClick(event) {
-    setActive(event.target.id)
-  }
+  const handleClick = (hash) => {
+    setActiveLink(hash);
+  };
   return (
     <section id="header-cont">
       <div className="logo">
@@ -20,67 +26,178 @@ export const Header = () => {
         </a>
       </div>
 
+      <div className="logo-tab">
+        <a href="#home" title="Home" className="vt">
+          VT
+        </a>
+      </div>
+
+      <div className="nav-cont-tab">
+      <ul className="nav-tab">
+        <li>
+          <a
+            href="#home"
+            title="Home"
+            className={activeLink === "#home" ? "active" : ""}
+            onClick={() => handleClick("#home")}
+          >
+            <FaHome />
+          </a>
+        </li>
+        <li>
+          <a
+            href="#about"
+            title="About"
+            className={activeLink === "#about" ? "active" : ""}
+            onClick={() => handleClick("#about")}
+          >
+            <CgProfile />
+          </a>
+        </li>
+        <li>
+          <a
+            href="#skills"
+            title="Skills"
+            className={activeLink === "#skills" ? "active" : ""}
+            onClick={() => handleClick("#skills")}
+          >
+            <HiMiniWrenchScrewdriver />
+          </a>
+        </li>
+        <li>
+          <a
+            href="#projects"
+            title="Projects"
+            className={activeLink === "#projects" ? "active" : ""}
+            onClick={() => handleClick("#projects")}
+          >
+            <FaBriefcase />
+          </a>
+        </li>
+        <li>
+          <a
+            href="#contact"
+            title="Contact"
+            className={activeLink === "#contact" ? "active" : ""}
+            onClick={() => handleClick("#contact")}
+          >
+            <MdContactMail />
+          </a>
+        </li>
+      </ul>
+    </div>
+
       {menu && (
         <div className="nav-cont">
-          <ul className="nav">
-            <li>
-              <a href="#home" title="Home">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#about" title="About">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#skills" title="Skills">
-                Skills
-              </a>
-            </li>
-            <li>
-              <a href="#projects" title="Projects">
-                Projects
-              </a>
-            </li>
-            <li>
-              <a href="#contact" title="Contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
+      <ul className="nav">
+        <li>
+          <a
+            href="#home"
+            title="Home"
+            className={activeLink === "#home" ? "active" : ""}
+            onClick={() => handleClick("#home")}
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a
+            href="#about"
+            title="About"
+            className={activeLink === "#about" ? "active" : ""}
+            onClick={() => handleClick("#about")}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="#skills"
+            title="Skills"
+            className={activeLink === "#skills" ? "active" : ""}
+            onClick={() => handleClick("#skills")}
+          >
+            Skills
+          </a>
+        </li>
+        <li>
+          <a
+            href="#projects"
+            title="Projects"
+            className={activeLink === "#projects" ? "active" : ""}
+            onClick={() => handleClick("#projects")}
+          >
+            Projects
+          </a>
+        </li>
+        <li>
+          <a
+            href="#contact"
+            title="Contact"
+            className={activeLink === "#contact" ? "active" : ""}
+            onClick={() => handleClick("#contact")}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
       )}
 
-      <div className="nav-cont-desk">
-        <ul className="nav-desk">
-          <li>
-            <a href="#home" title="Home" id='1' className={active === '1' ? 'active' : ''} onClick={handleClick}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#about" title="About">
-              About
-            </a>
-          </li>
-          <li>
-              <a href="#skills" title="Skills">
-                Skills
-              </a>
-          </li>
-          <li>
-            <a href="#projects" title="Projects">
-              Projects
-            </a>
-          </li>
-          <li>
-            <a href="#contact" title="Contact">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
+<div className="nav-cont-desk">
+      <ul className="nav-desk">
+        <li>
+          <a
+            href="#home"
+            title="Home"
+            className={activeLink === "#home" ? "active" : ""}
+            onClick={() => handleClick("#home")}
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a
+            href="#about"
+            title="About"
+            className={activeLink === "#about" ? "active" : ""}
+            onClick={() => handleClick("#about")}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="#skills"
+            title="Skills"
+            className={activeLink === "#skills" ? "active" : ""}
+            onClick={() => handleClick("#skills")}
+          >
+            Skills
+          </a>
+        </li>
+        <li>
+          <a
+            href="#projects"
+            title="Projects"
+            className={activeLink === "#projects" ? "active" : ""}
+            onClick={() => handleClick("#projects")}
+          >
+            Projects
+          </a>
+        </li>
+        <li>
+          <a
+            href="#contact"
+            title="Contact"
+            className={activeLink === "#contact" ? "active" : ""}
+            onClick={() => handleClick("#contact")}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
 
       <div className="mode-cont">
         <BsFillMoonStarsFill className="mode-icon" />
@@ -97,11 +214,16 @@ export const Header = () => {
       </div>
 
       <div className="menu-cont">
+        {menu === true ?<IoMdClose  
+        className="menu-icon"
+        onClick={() => setMenu(!menu)}
+        title="Menu"
+        /> : 
         <RiMenu2Fill
           className="menu-icon"
           onClick={() => setMenu(!menu)}
           title="Menu"
-        />
+        />}
       </div>
     </section>
   );
